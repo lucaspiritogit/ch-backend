502953de1efed99d793659aa758ad259a85084ef
lpirito <lucasignaciopirito@gmail.com>
2022-07-31 01:01:04 -0300

    avances iniciales

---
+++ clase4.js
@@ -0,0 +1,67 @@
+const fs = require("fs");
+
+class Container {
+  constructor(filePath) {
+    this.filePath = filePath;
+  }
+
+  startDocument() {
+    const readFile = fs.readFileSync(this.filePath, "utf-8");
+    if (readFile.length === 0) {
+      fs.writeFileSync(this.filePath, JSON.stringify([], null, 2), "utf-8");
+    } else {
+      return;
+    }
+  }
+
+  async save(object) {
+    try {
+      let fileData = JSON.parse(fs.readFileSync(this.filePath, "utf-8"));
+      await fileData.push(object);
+
+      fileData.forEach((element, index) => {
+        element.id = index;
+      });
+
+      await fs.promises.writeFile(this.filePath, JSON.stringify(fileData, null, 2), "utf-8");
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  getById(objectId) {
+    let fileData = JSON.parse(fs.readFileSync(this.filePath, "utf-8"));
+    fileData.forEach(element => {
+      if (element.id === objectId) {
+        console.log(element);
+        return element;
+      }
+    });
+  }
+
+  getAll() {
+    return fs.readFileSync(this.filePath, "utf-8");
+  }
+
+  deleteById(objectId) {}
+
+  deleteAll() {
+    fs.writeFileSync(this.filePath, "", "utf-8");
+  }
+}
+
+const container = new Container("./productos.txt");
+container.startDocument();
+container.save({ title: "Product", price: 100, thumbnail: "url" });
+setTimeout(() => {
+  container.getAll();
+}, 500);
+
+// Correr solo este metodo para encontrar un elemento
+// container.getById(6);
+
+// Correr solo este metodo para borrar un elemento
+// container.deleteById(0);
+
+// Correr solo este metodo para limpiar el documento
+// container.deleteAll();
