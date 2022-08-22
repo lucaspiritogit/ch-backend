68f231b8446c9eecf0e9f314879026a0cb4ce593
lpirito <lucasignaciopirito@gmail.com>
2022-08-22 19:45:19 -0300

    linked css to files

--- src/api/routes/productos.routes.js
+++ src/api/routes/productos.routes.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const app = express();
+const path = require("path");
 const Container = require("../service/Container.js");
 const routerProductos = express.Router();
 
