b104377a4cadfe5ce51f11315ce2ab99279927ab
lpirito <lucasignaciopirito@gmail.com>
2022-08-27 22:52:00 -0300

    using pug

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
-const exphbs = require("express-handlebars");
 const app = express();
-const routerProductos = require("./src/api/routes/productos.routes.js");
 const path = require("path");
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+const routerProductos = require("./src/api/routes/productos.routes.js");
 const Container = require("./src/api/service/Container.js");
 
 const PORT = 8080;
@@ -15,14 +19,18 @@ app.use(express.static(path.join(__dirname, "./src/api/public")));
 app.set("views", path.join(__dirname + "/views"));
 app.set("view engine", "pug");
 
-app.listen(PORT, () => {
-  console.log(`Server corriendo en http://localhost:${PORT}`);
-});
-
 app.use("/api/productos", routerProductos);
 
 app.get("/", (req, res) => {
   res.render("formPost.pug");
+});
+
+io.on("connection", socket => {
+  console.log(`Conectado: ${socket.id}`);
+});
+
+server.listen(PORT, () => {
+  console.log(`Server corriendo en http://localhost:${PORT}`);
 });
 
 // glitch sv: https://glitch.com/edit/#!/oval-victorious-peridot?path=server%2Fapp.js%3A14%3A23
