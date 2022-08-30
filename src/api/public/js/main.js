c7a62d388ff1daa411bd44f1d5c17da7789be238
lpirito <lucasignaciopirito@gmail.com>
2022-08-30 19:45:08 -0300

    fixing chat

--- src/api/public/js/main.js
+++ src/api/public/js/main.js
@@ -23,7 +23,6 @@ socket.on("productos", productos => {
 });
 
 socket.on("nuevo-mensaje-server", mensajes => {
-  console.log(mensajes);
   render(mensajes);
 });
 
@@ -31,22 +30,28 @@ function render(mensajes) {
   const html = mensajes
     .map(msj => {
       return `
-         <h3>Usuario: ${msj.usuario}</h3>
-          <h3>Mensaje: ${msj.mensaje}</h3>                        
-         `;
+      <div class="mensajes">
+        <h3 class="usuario">${msj.usuario}:</h3>
+        <h3 class="mensaje">&nbsp${msj.mensaje}&nbsp</h3>
+        <h3 class="timestamp">| [${msj.timestamp}]</h3>                        
+       </div
+        `;
     })
     .join("<br>");
 
   document.getElementById("chatLog").innerHTML = html;
 }
 
-const enviarMensaje = () => {
-  const inputUsuario = document.getElementById("usuario");
+const enviarMensaje = e => {
+  const inputEmail = document.getElementById("email");
   const inputMensaje = document.getElementById("mensaje");
+  const time = new Date();
 
   const mensaje = {
-    usuario: inputUsuario.value,
+    usuario: inputEmail.value,
     mensaje: inputMensaje.value,
+    timestamp: time.toLocaleDateString() + " " + time.toLocaleTimeString(),
   };
   socket.emit("nuevo-mensaje-cliente", mensaje);
+  return false;
 };
