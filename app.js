e8b13c9ce8254c1f24f9aeb42fa8142f550b4f01
lpirito <lucasignaciopirito@gmail.com>
2022-08-30 18:55:39 -0300

    adding css to pug

--- app.js
+++ app.js
@@ -10,11 +10,15 @@ const PORT = 8080;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "./src/api/public")));
-app.set("views", path.join(__dirname + "/views"));
-app.set("view engine", "ejs");
+
+// app.set("views", path.join(__dirname + "/views"));
+// app.set("view engine", "ejs");
 
 // app.set("views", path.join(__dirname + "/views"));
 // app.set("view engine", "hbs");
+
+app.set("views", path.join(__dirname + "/views"));
+app.set("view engine", "pug");
 
 app.engine(
   "hbs",
