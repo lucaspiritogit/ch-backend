e7af39c59183397fdc97143098c56c94cb1d6c04
lpirito <lucasignaciopirito@gmail.com>
2022-10-23 21:33:16 -0300

    test

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
+import * as dotenv from "dotenv";
 import express, { json, urlencoded } from "express";
 import { engine } from "express-handlebars";
 import { Server as HttpServer } from "http";
-import { join } from "path";
+import { Server as IOServer } from "socket.io";
+import * as url from "url";
+import { productoDao } from "./src/api/dao/setDB.js";
+dotenv.config();
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
 
 const app = express();
 const server = new HttpServer(app);
+const io = new IOServer(server);
 
 const PORT = 8080;
 
 /* ---------------------------- Middlewares ------------------------- */
 app.use(json());
 app.use(urlencoded({ extended: true }));
-
+app.use(express.static(__dirname + "src/api/public"));
 /* ---------------------------- Views ------------------------- */
-app.set("views", join("/views"));
 app.set("view engine", "hbs");
 
 app.engine(
@@ -31,6 +37,33 @@ import routerProductos from "./src/api/routes/productos.routes.js";
 
 app.use("/api/productos", routerProductos);
 app.use("/api/carrito", routerCarrito);
+
+app.get("/", (req, res) => {
+  res.render("./index.hbs");
+});
+
+/* --------------------------- SocketIO ---------------------------------- */
+io.on("connection", async socket => {
+  socket.on("productos-cliente", data => {
+    productoDao.save(data);
+    io.sockets.emit("productos-server", productoDao.getAll());
+  });
+
+  socket.emit("productos-server", productoDao.getAll());
+
+  let mensajesArray = [];
+  socket.on("nuevo-mensaje-cliente", async data => {
+    try {
+      mensajes.save(data);
+      mensajesArray.push(data);
+      io.sockets.emit("nuevo-mensaje-server", mensajesArray);
+    } catch (error) {
+      throw error;
+    }
+  });
+
+  socket.emit("nuevo-mensaje-server", mensajesArray);
+});
 
 server.listen(PORT, () => {
   console.log(`Server up at http://localhost:${PORT}`);
--- src/api/public/js/sendProduct.js
+++ src/api/public/js/sendProduct.js
@@ -21,27 +21,27 @@ const sendProduct = () => {
 };
 
 function renderProducts(productos) {
-  const html = productos
-    .map(product => {
-      if (product.price == null) {
-        product.price = `Valor no ingresado`;
-      }
-      return `
-    <div class="producto">
-      <h3>Id: ${product.id}</h3> 
-      <h3>Titulo:${product.title}</h3>
-      <h3>Precio:${product.price}</h3>
-      <div class="productoThumbnail">
-        <h3>Image:</h3><img  src="${product.thumbnail}"/>
-      </div>
-      <h3>Stock: ${product.stock}</h3>
-      <h3>Code: ${product.code}</h3>
-      <h3>Description: ${product.description}</h3>
-    </div>
-    `;
-    })
-    .join("");
-  document.querySelector("#listadoProductos").innerHTML = html;
+  // const html = productos
+  //   .map(product => {
+  //     if (product.price == null) {
+  //       product.price = `Valor no ingresado`;
+  //     }
+  //     return `
+  //   <div class="producto">
+  //     <h3>Id: ${product.id}</h3>
+  //     <h3>Titulo:${product.title}</h3>
+  //     <h3>Precio:${product.price}</h3>
+  //     <div class="productoThumbnail">
+  //       <h3>Image:</h3><img  src="${product.thumbnail}"/>
+  //     </div>
+  //     <h3>Stock: ${product.stock}</h3>
+  //     <h3>Code: ${product.code}</h3>
+  //     <h3>Description: ${product.description}</h3>
+  //   </div>
+  //   `;
+  //   })
+  //   .join("");
+  // document.querySelector("#listadoProductos").innerHTML = html;
 }
 
 // When products are received, render them in HTML format.
