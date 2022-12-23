cb91dffe72dc31b2c021822fa0bbaed2833a0661
lpirito <lucasignaciopirito@gmail.com>
2022-12-22 22:28:03 -0300

    mochawesome to generate reports and better tests

--- src/test/productos.test.js
+++ src/test/productos.test.js
@@ -1,13 +1,14 @@
 const axios = require("axios");
 const assert = require("assert").strict;
-
+const ProductService = require("../api/service/ProductService.js");
 const url = "http://localhost:8080/api/productos";
 
+let productService = new ProductService();
 /*
     Controller of products test
 */
 
-describe("Test de productos", () => {
+describe("Product tests", () => {
   // All products
   it("GET /productos", async () => {
     const response = await axios.get(url);
@@ -16,7 +17,17 @@ describe("Test de productos", () => {
 
   // Single product
   it("GET /productos/:id", async () => {
-    const response = await axios.get(`${url}/1`);
+    let products = await productService.getAllProducts();
+
+    let productsIds = [];
+    products.forEach(async (product) => {
+      productsIds.push(product.id);
+    });
+
+    let randomProductId =
+      productsIds[Math.floor(Math.random() * productsIds.length)];
+
+    const response = await axios.get(`${url}/${randomProductId}`);
     assert.equal(response.status, 200);
   });
 
@@ -40,5 +51,18 @@ describe("Test de productos", () => {
       }
     );
     assert.equal(response.status, 201);
+  });
+
+  // Update product
+  it("PUT /productos/:id", async () => {
+    const response = await axios.put(`${url}/1`, {
+      title: "Test",
+      price: 100,
+      description: "Test",
+      code: "code",
+      thumbnail: "thumbnail",
+      stock: 100,
+    });
+    assert.equal(response.status, 200);
   });
 });
